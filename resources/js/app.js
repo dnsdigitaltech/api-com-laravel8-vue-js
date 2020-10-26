d36bff2638c5278f2271c5f3a39cad34f64f42c3
dnsdigitaltech <dnssites@gmail.com>
2020-10-26 10:52:44 -0300

    consigurando o VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,8 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+import router from './routes/routes'
 
 /**
  * Components globais
@@ -14,6 +10,7 @@ window.Vue = require('vue');
 Vue.component('test-component', require('./components/TestComponent.vue').default)
 
 const app = new Vue({
+    router,
     el: '#app',
 });
 
